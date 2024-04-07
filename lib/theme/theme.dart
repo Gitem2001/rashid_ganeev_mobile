949a98e9ab052235bbdd546277a912be2cda2185
rashidganeev <ganeev.r@outlook.com>
2024-04-07 20:48:22 +1000

    Create registration window. Dont use state var Dont work update data from fields

---
+++ lib/theme/theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  brightness: Brightness.light,
+  colorScheme: ColorScheme.light(
+    background: Colors.red,
+  )
+);
+
+ThemeData darkMode = ThemeData(
+    brightness: Brightness.light,
+    colorScheme: ColorScheme.light(
+      background: Colors.green,
+    )
+);
