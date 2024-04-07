8ea00aa333dfcd7210908e62c0047121e1ba028f
rashidganeev <ganeev.r@outlook.com>
2024-04-08 00:05:55 +1000

    Restructure project

---
+++ lib/Functions/appin.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:lab1_hope2/UI/homepage.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ganeev_app_example',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Registration'),
+    );
+  }
+}
